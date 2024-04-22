1dde319600149883429738139a553ae186831fc1
Boris Knezevic <boris.knezevic0@gmail.com>
2024-04-22 14:52:21 +0200

    add CSV recap

--- lectures/modeling-data/patient_repository.rb
+++ lectures/modeling-data/patient_repository.rb
@@ -18,22 +18,40 @@ class PatientRepository
     patient.id = @next_id
     @next_id += 1
     @patients << patient
+    # save_csv
   end
 
   def load_csv
-    CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |row|
-      # row => {name: "Nicole", cured: "true"...}, 
-      row[:id] = row[:id].to_i
-      row[:cured] = row[:cured] == "true"
+    # CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |row|
+    #   # row => {name: "Nicole", cured: "true"...}, 
+    #   row[:id] = row[:id].to_i
+    #   row[:cured] = row[:cured] == "true"
 
-      room_id = row[:room_id].to_i
-      room = @room_repository.find(room_id)
+    #   room_id = row[:room_id].to_i
+    #   room = @room_repository.find(room_id)
       
+    #   patient = Patient.new(row)
+    #   patient.room = room
+    #   @patients << patient
+    #   room.add_patient(patient)
+    # end
+    # @next_id = @patients.last.id + 1 if @patients.any?
+    CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |row|
+      row[:cured] = row[:cured] == "true"
+      row[:id] = row[:id].to_i
+
       patient = Patient.new(row)
-      patient.room = room
       @patients << patient
-      room.add_patient(patient)
     end
-    @next_id = @patients.last.id + 1 if @patients.any?
+  end
+
+  def save_csv
+    CSV.open(@csv_file_path, "wb") do |csv|
+      csv << ["id", "name", "cured"]
+      @patients.each do |patient|
+        csv << [patient.id, patient.name, patient.cured]
+      end
+    end
   end
 end
+
